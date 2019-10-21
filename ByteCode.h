bdb6929252f6836fcbf02fe8d8c77bdf37946e96
Daryl D'Abreo <daryldabreo@pal-nat186-29-50.itap.purdue.edu>
2019-10-21 15:11:00 -0400

    Initialize Main

--- ByteCode.h
+++ ByteCode.h
@@ -1,7 +1,8 @@
 //
 // Created by Ethan Masunaga and daryl on 10/21/19.
 //
-
+#include <iostream>
+using namespace std;
 
 class ByteCode {
 public:
--- main.cpp
+++ main.cpp
@@ -6,4 +6,14 @@
 
 int main(int argc, char **argv) {
 
+    // rstack = vector that contains data items
+    int sp = -1; //top of runtime stack
+
+    //fpstack = first element in function stack frame
+    int fpsp = -1; //top of frame stack
+
+    int pc = 0; //contains address of next instruction
+    //mem = array of bytes
+
+
 }
